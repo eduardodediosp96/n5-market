7c25e9b94e4a6181203cd1cc9debc1589ff75fa6
Eduardo Dedios <eduardodediosp96@gmail.com>
2024-07-01 12:15:59 -0500

    Add theme config for Emotion, Web Vitals setup, and global CSS

---
+++ src/common/Theme/Breakpoints.ts
@@ -0,0 +1,26 @@
+// @Types
+import { Breakpoint, BreakpointsType } from './Theme.types';
+
+const breakpointValues = {
+  mobileS: 320,
+  mobileL: 426,
+  tabletS: 600,
+  tablet: 768,
+  laptopS: 1024,
+  laptopXL: 2560,
+};
+
+export const breakpoints: BreakpointsType = {
+  values: breakpointValues,
+  up: (breakpoint: string) => {
+    return `@media (min-width:${breakpointValues[breakpoint as Breakpoint]}px)`;
+  },
+  down: (breakpoint: string) => {
+    return `@media (max-width:${breakpointValues[breakpoint as Breakpoint]}px)`;
+  },
+  between: (start: string, end: string) => {
+    return `@media (min-width:${
+      breakpointValues[start as Breakpoint]
+    }px) and (max-width:${breakpointValues[end as Breakpoint]}px)`;
+  },
+};
